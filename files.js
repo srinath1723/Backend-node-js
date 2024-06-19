b8e198262033981bae3bb762b10ff1c9259ba10a
srinath1723 <srinathsp890.com>
2024-06-19 12:35:17 +0530

    unlink -to delete the file

--- files.js
+++ files.js
@@ -1,12 +1,12 @@
 const fs=require('fs')
 
-fs.readFile("hello.text",'utf-8',(err,data)=>{
+fs.unlink("hello.text",(err)=>{
     if(err){
-        console.log('error reading file',err);
+        console.log(err);
     }
     else{
-        data.split("\n").forEach(line=>{
-        console.log(line.split(':')[1]);
-    })
+        
+        console.log('File deleted');
+    
     }
 });
