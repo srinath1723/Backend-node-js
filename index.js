4390470d27f968a246bdfadadad0a200a3529280
srinath1723 <srinathsp890.com>
2024-06-20 11:43:40 +0530

    basic express server --- get method

---
+++ index.js
@@ -0,0 +1,14 @@
+// 1. import express module
+const express= require('express');
+
+//2. create express app
+const app=express();
+//4.cretae a route
+app.get('/',(request,response)=>{
+     response.send('hello world!');
+})
+
+//3. run the server
+app.listen(3002,()=>{
+   console.log('server is rinning on http://localhost:3002');
+});
