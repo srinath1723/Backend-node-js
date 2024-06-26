8c78262aaa7683a31716a5b2ccfb198ac5fb91aa
srinath1723 <srinathsp890.com>
2024-06-26 16:36:42 +0530

    company route added --add company --get all companies

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const userRouter = require('./routes/userRoutes');
+const companyRouter = require('./routes/companyRoutes');
 
-const userRouter= require('./routes/userRoutes');
 const app = express();
+
 app.use(express.json());
 
-app.use('/users',userRouter);
-module.exports=app;
+app.use('/users', userRouter);
+app.use('/', companyRouter);
+
+module.exports = app;
--- models/user.js
+++ models/user.js
@@ -6,4 +6,4 @@ const userSchema = new mongoose.Schema({
     password: String,
 });
 
-module.exports = mongoose.model('user', userSchema, 'users');
+module.exports = mongoose.model('User', userSchema, 'users');
