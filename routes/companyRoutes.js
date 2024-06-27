b11e45f6eb04d11d4aff7ff7e76992de7d4f6dda
srinath1723 <srinathsp890.com>
2024-06-27 12:42:39 +0530

    updateUser & deleteUser endpoints done

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -3,8 +3,9 @@ const companyRouter = express.Router();
 const companyController = require('../controllers/companyController');
 const auth = require('../middleware/auth');
 
-
+//public routes
 companyRouter.get('/companies', companyController.getAllCompanies);
+//protected routes
 // define the route for adding a company
 companyRouter.post('/users/:userId/companies',auth.verifyToken, companyController.addCompany);
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,13 +4,14 @@ const userController = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
 
-
+//public routes
 userRouter.post('/', userController.register);
 userRouter.post('/login', userController.login);
-
+//protected routes
 // define the route for getting all users
 userRouter.get('/', auth.verifyToken, userController.getAllUsers);
 userRouter.get('/logout',auth.verifyToken, userController.logout);
 userRouter.get('/:id',auth.verifyToken, userController.getUserById);
-
+userRouter.put('/:id', auth.verifyToken, userController.updateUser);
+userRouter.delete('/:id', auth.verifyToken, userController.deleteUser);
 module.exports = userRouter;
